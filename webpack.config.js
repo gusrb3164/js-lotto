658fe6bbf85bd09d3d267bf1012f947af2fdd2fb
gusrb3164 <gusrb3164@gmail.com>
2023-08-20 17:45:56 +0900

    chore: 웹 기반 webpack 환경 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = env => ({
   entry: env.entry,
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".mjs"],
+    extensions: ['.js', '.mjs'],
   },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
-  devtool: "inline-source-map",
-  target: "node",
+  plugins: [new CleanWebpackPlugin(), new HtmlPlugin({template: './index.html'})],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    host: 'localhost',
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
 });
